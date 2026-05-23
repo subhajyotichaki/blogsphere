f225a22a32cb568aa97425756f339e19b6a9e687
subhajyotichaki <subhajyotichaki14@gmail.com>
2026-05-24 01:20:51 +0530

    re

--- blog-api/routes/authRoutes.js
+++ blog-api/routes/authRoutes.js
@@ -1,19 +1,14 @@
 const express = require("express");
 
 const {
-    registerUser,
-    loginUser,
+  registerUser,
+  loginUser,
 } = require("../controllers/authController");
 
 const router = express.Router();
 
-
-// REGISTER
 router.post("/register", registerUser);
 
-
-// LOGIN
 router.post("/login", loginUser);
-
 
 module.exports = router;
