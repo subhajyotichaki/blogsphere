60c75af7924a80732eb7d1ea142e2c4a0c55118b
subhajyotichaki <subhajyotichaki14@gmail.com>
2026-05-24 15:20:48 +0530

    resdcs\\

--- blog-api/server.js
+++ blog-api/server.js
@@ -12,11 +12,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "https://blogsphere-umber.vercel.app",
+    credentials: true,
   })
 );
+
+app.options(/.*/, cors());
 
 app.use(express.json());
 
