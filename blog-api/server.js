26bf2961c0225ccf8427aee6c246efa596f7d1b4
subhajyotichaki <subhajyotichaki14@gmail.com>
2026-05-29 17:00:05 +0530

    fix auth and coris

--- blog-api/server.js
+++ blog-api/server.js
@@ -4,37 +4,81 @@ const cors = require("cors");
 
 const connectDB = require("./config/db");
 
+
 dotenv.config();
+
 
 connectDB();
 
+
 const app = express();
 
+
+// CORS CONFIG
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://blogsphere-umber.vercel.app",
-    "https://blogsphere-qbbqvjxeg-subhajyotichakis-projects.vercel.app",
-    "https://blogsphere-44x27aox-subhajyotichakis-projects.vercel.app",
+
+  origin: function (origin, callback) {
+
+    if (
+      !origin ||
+      origin.includes("localhost") ||
+      origin.endsWith(".vercel.app")
+    ) {
+
+      callback(null, true);
+
+    } else {
+
+      callback(new Error("Not allowed by CORS"));
+
+    }
+
+  },
+
+  methods: [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "OPTIONS",
   ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+  ],
+
   credentials: true,
+
 };
+
 
 app.use(cors(corsOptions));
 
+
+// Middleware
 app.use(express.json());
 
+
+// Routes
 app.use("/api/blogs", require("./routes/blogRoutes"));
+
 app.use("/api/auth", require("./routes/authRoutes"));
 
+
+// Test Route
 app.get("/", (req, res) => {
+
   res.send("Blog API Running");
+
 });
+
 
 const PORT = process.env.PORT || 5000;
 
+
 app.listen(PORT, () => {
+
   console.log(`Server running on port ${PORT}`);
+
 });
